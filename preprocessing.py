91798d0ce5d26cd527db1261a1e8224a37a4de89
Muhammad Irfan Danish <irdanish11@gmail.com>
2020-06-16 17:43:23 +0500

    updated preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -6,3 +6,43 @@ Created on Mon Jun 15 12:25:17 2020
 """
 
 
+import pandas as pd
+
+covid_df = pd.read_csv('./Data/COVIDData.csv')
+mobility_df = pd.read_csv('./Data/Mobility_eth.csv')
+sales_df = pd.read_csv('./Data/SalesData.csv')
+foot_traffic = pd.read_csv('./Data/CustomerFootTraffic.csv')
+
+div_num_sales = sales_df.div_nbr.unique()
+div_num_trafic = foot_traffic.div_nbr.unique()
+
+#checking the for same div_num in sales_df & foot_traffic
+same_div_num = []
+for i in div_num_sales:
+    if i in div_num_trafic:
+        same_div_num.append(i)
+        
+#checking the median dwell
+data = {}
+for i in range(len(foot_traffic.div_nbr)):
+    if foot_traffic.div_nbr[i] == 2020:
+        data[weeks[i]] = foot_traffic.median_dwell[i]
+        #data.append(foot_traffic.median_dwell[i])
+        
+        
+def to_weeks(obj, splitter='/', convet=True):       
+    if convert:
+        for j in range(len(obj)):
+            split = obj[j].split(splitter)
+            new = ['0'+i if len(i)<2  else i for i in split]
+            if len(new[2])<4:
+                new[2] = new[2]+'20'
+            obj[j] = ''.join(new)
+    obj = pd.to_datetime(obj, format='%m%d%Y')
+    return obj.dt.week
+    
+#Adds the column number of weeks by converting date into week number     
+foot_traffic['fisc_yr_wk'] = to_weeks(obj=foot_traffic.clndr_dt)
+
+
+
