85e7ed46398d44d7518bf7f6a4246db55fa818c5
Muhammad Irfan Danish <irdanish11@gmail.com>
2020-06-28 18:35:09 +0500

    extracted data for non covid weeks

--- covid_SalesTraffic_mapping.py
+++ covid_SalesTraffic_mapping.py
@@ -8,6 +8,7 @@ Created on Sun Jun 28 16:59:59 2020
 
 import pandas as pd
 import numpy as np
+from utilities import extract_sub_df
 
 path = r'C:\Users\danis\Documents\USFoods'
 df_covid = pd.read_csv(path+'/Data/Processed_COVID.csv')
@@ -25,3 +26,28 @@ weeks_not_cmn = np.setxor1d(weeks_cmn, weeks_sales_tr)
 #extracting the weeks that does not have covid data, which are non-common weeks
 df_sales_tr['fisc_wk2'] = df_sales_tr['fisc_wk']
 df_sales_tr = df_sales_tr.set_index('fisc_wk2').sort_index()
+
+#constructing df for non covid weeks
+df_sales_Ncovid = []
+for w in weeks_not_cmn:
+    df_sales_Ncovid.append(extract_sub_df(df_sales_tr, w))
+df_sales_Ncovid = pd.concat(df_sales_Ncovid) 
+#creating the covid data for non-covid weeks that is why just adding 0's   
+df_sales_Ncovid['cases'] = np.arange(0, len(df_sales_Ncovid))*0
+df_sales_Ncovid['mortality'] = np.arange(0, len(df_sales_Ncovid))*0
+
+
+################ Mapping covid features to SalesTraffic Data ################
+
+
+
+
+
+
+
+
+
+
+
+
+
