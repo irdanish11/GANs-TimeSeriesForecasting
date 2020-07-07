14b27ef541e769b321db7c100caac0147a305411
Muhammad Irfan Danish <irdanish11@gmail.com>
2020-07-07 22:17:21 +0500

    update mobility mapping

--- preprocessing_Mobility_SalesTraffic.py
+++ preprocessing_Mobility_SalesTraffic.py
@@ -11,11 +11,12 @@ from utilities import to_weeks, extract_sub_df
 import os
 import numpy as np
 import csv
+from tqdm import tqdm
 
 path = r'C:\Users\danis\Documents\USFoods'
 
 #Getting Sales & Traffic Data
-df_sales_tr = pd.read_csv(path+'/Data/Sales_Traffic_Mapping.csv')
+df_sales_tr = pd.read_csv(path+'/Data/Sales_Traffic_Covid_Mapping.csv')
 zip_cd_sl = np.sort(df_sales_tr.zip_cd.unique())
 
 
@@ -26,8 +27,8 @@ final_df = []
 for f in csv_files:
     print('Reading the CSV file!')
     mobility_df = pd.read_csv(path2+'/'+f)
-    mobility_df = mobility_df[['postal_code', 'date_range_start', 'date_range_end', 
-                                   'raw_visit_counts', 'raw_visitor_counts']]
+    mobility_df = mobility_df[['postal_code', 'date_range_start', 'raw_visit_counts',
+                               'raw_visitor_counts', 'median_dwell']]
     print('Processing the CSV file!')
     df_mb = mobility_df.head()
     zip_cd_mb = np.sort(mobility_df.postal_code.unique())
@@ -56,12 +57,41 @@ final_df = final_df.reset_index(drop=True)
 df_sales_tr['fisc_wk2'] = df_sales_tr['fisc_wk']
 df_sales_tr = df_sales_tr.set_index('fisc_wk2').sort_index()
 dfs = []
-for w in [17, 18]:
-    dfs.append(df_sales_tr.loc[w])
-    
-    
-    
-    
-    
-    
-    
+cmn_weeks = np. intersect1d(final_df.fisc_wk.unique(), df_sales_tr.fisc_wk.unique())
+final_df = final_df.set_index('fisc_wk2').sort_index()
+
+################# Mapping the Data #################
+dfs_1 = []
+for w in tqdm(cmn_weeks):
+    df_st = extract_sub_df(df_sales_tr, w)
+    df_st['zip_cd2'] = df_st['zip_cd']
+    df_mob = extract_sub_df(final_df, w)
+    df_mob['zip_cd2'] = df_mob['zip_cd']
+    #setting index
+    df_st = df_st.set_index('zip_cd2').sort_index()
+    df_mob = df_mob.set_index('zip_cd2').sort_index() 
+    cmn_zips = np.intersect1d(df_st.zip_cd.unique(), df_mob.zip_cd.unique())
+    dfs_2 = []
+    for z in cmn_zips:
+        df1_s = extract_sub_df(df_st, z)
+        df2_m = extract_sub_df(df_mob, z)
+        if len(df1_s) < len(df2_m):
+                size = len(df1_s)
+        else:
+            size = len(df2_m)
+        df1_s = df1_s.reset_index(drop=True)
+        df2_m = df2_m.reset_index(drop=True)
+        df1_s = df1_s.loc[0:size-1]
+        df2_m = df2_m.loc[0:size-1]
+        #Adding matched entries
+        df1_s['raw_visit_counts'] = df2_m['raw_visit_counts']
+        df1_s['raw_visitor_counts'] = df2_m['raw_visitor_counts']
+        df1_s['mob_median_dwell'] = df2_m['median_dwell']
+        dfs_2.append(df1_s)
+    dfs_1.append(pd.concat(dfs_2))
+
+mapped_df = pd.concat(dfs_1)    
+mapped_df = mapped_df.reset_index(drop=True).sort_index()    
+
+mapped_df.to_csv(path+'/Data/Sales_Traffic_Covid_Mobility_Mapping.csv', index=False)    
+
